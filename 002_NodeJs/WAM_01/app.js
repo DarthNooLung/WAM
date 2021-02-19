e1f9a747b9f36d43abed3bf548e7847f0fbe7cfa
DarthNooLung <47206477+DarthNooLung@users.noreply.github.com>
2021-02-19 21:45:14 +0900

    프로젝트 기본 생성

---
+++ 002_NodeJs/WAM_01/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require("dotenv");
+const path = require('path');
+
+dotenv.config();
+
+const indexRouter =require('./routes');
+const userRouter =require('./routes/user');
+
+const app = express();
+app.set('port', process.env.PORT);
+
+app.use(morgan('dev'));
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중');
+});
