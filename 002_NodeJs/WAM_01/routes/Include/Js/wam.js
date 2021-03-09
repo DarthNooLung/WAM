9f241c8208fa14520b17d39120221d4812195fe3
DarthNooLung <47206477+DarthNooLung@users.noreply.github.com>
2021-03-09 12:58:16 +0900

    202103091300
    호출 함수 구현

--- 002_NodeJs/WAM_01/routes/Include/Js/wam.js
+++ 002_NodeJs/WAM_01/routes/Include/Js/wam.js
@@ -1,1 +1,26 @@
+const { escapeSelector } = require("jquery");
+
 var WaitAMinute = new Object();
+
+function WAM_GO(o, c)
+{
+    if(o == undefined)
+    {
+        alert("없어1");
+        return false;
+    }
+    else
+    {
+        alert("있어1");
+        alert(o.wam_id);
+    }
+
+    if(c == undefined)
+    {
+        alert("없어2");
+    else 
+    {
+        alert("있어2");
+        c();
+    }
+}
