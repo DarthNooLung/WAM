20616c2dfdaf2bf82941b43cb4939bfe883fe190
DarthNooLung <47206477+DarthNooLung@users.noreply.github.com>
2021-03-11 15:32:06 +0900

    202103111530
    레이어 작업

--- 002_NodeJs/WAM_01/routes/Include/Js/wam.js
+++ 002_NodeJs/WAM_01/routes/Include/Js/wam.js
@@ -5,8 +5,13 @@ var WaitAMinute = new Object();
 
 //ActionId
 WaitAMinute.ActionId = "";
+//Action 경로
+WaitAMinute.URL = "http://localhost:216";
 //호출 본체 함수
 //WaitAMinute.CallBodyFunction = null;
+//카운팅 레이어 크기
+WaitAMinute.MainLayerX = 300;
+WaitAMinute.MainLayerY = 200;
 
 function WAM_GO(o, b)
 {
@@ -22,6 +27,44 @@ function WAM_GO(o, b)
         }
     }
 
+    var mBody = document;
+
+    if(mBody != null){
+        //마스크 프레임 추가
+        if(mBody.getElementById("wamMaskFrame") == null){
+            var cFrameLayer = mBody.createElement("iframe");
+            cFrameLayer.id = "wamMaskFrame";
+            cFrameLayer.style.position = "absolute";
+            cFrameLayer.style.width = "100%";
+            cFrameLayer.style.height = "100%";
+            cFrameLayer.style.border = "0px";
+            cFrameLayer.style.top = "0px";
+            cFrameLayer.style.left = "0px";
+            cFrameLayer.style.backgroundColor = "black";
+            cFrameLayer.style.opacity = 0.2;
+            cFrameLayer.style.display = "none";
+            cFrameLayer.style.zIndex = 9999;
+            cFrameLayer.src = "about:blank";
+            mBody.getElementsByTagName("body")[0].appendChild(cFrameLayer);
+        }
+        
+        if(mBody.getElementById("wamMainFrame") == null){
+            var cFrameLayer = mBody.createElement("iframe");
+            cFrameLayer.id = "wamMainFrame";
+            cFrameLayer.style.position = "absolute";
+            cFrameLayer.style.width = String(WaitAMinute.MainLayerX) + "px";
+            cFrameLayer.style.height = String(WaitAMinute.MainLayerY) + "px";
+            cFrameLayer.style.border = "0px";
+            cFrameLayer.style.top = "0px";
+            cFrameLayer.style.left = "0px";
+            cFrameLayer.style.backgroundColor = "black";
+            cFrameLayer.style.display = "none";
+            cFrameLayer.style.zIndex = 99999;
+            cFrameLayer.src = "about:blank";
+            mBody.getElementsByTagName("body")[0].appendChild(cFrameLayer);
+        }
+    }
+
     //설정값이 있는지 체크
     if(o == undefined)
     {
@@ -34,7 +77,6 @@ function WAM_GO(o, b)
         }
         else {
             WaitAMinute.ActionId = o.action_id;
-
             wamStart();
         }
     }
@@ -43,7 +85,7 @@ function WAM_GO(o, b)
 function wamStart() {
     $("#btnStart").hide();
     $.ajax({
-        url: "http://localhost:216/wams?ActionId=" + WaitAMinute.ActionId,
+        url: WaitAMinute.URL + "/wams?ActionId=" + WaitAMinute.ActionId,
         dataType: 'jsonp',
         jsonp: 'jsonp',
         jsonpCallback: "wamResult",
@@ -55,7 +97,7 @@ function wamStart() {
 
 function wamWait() {
     $.ajax({
-        url: "http://localhost:216/wami?ActionId=" + WaitAMinute.ActionId,
+        url: WaitAMinute.URL + "/wami?ActionId=" + WaitAMinute.ActionId,
         dataType: 'jsonp',
         jsonp: 'jsonp',
         jsonpCallback: "wamResult",
@@ -67,7 +109,7 @@ function wamWait() {
 
 function wamFinish() {
     $.ajax({
-        url: "http://localhost:216/wamf?ActionId=1",
+        url: WaitAMinute.URL + "/wamf?ActionId=1",
         dataType: 'jsonp',
         jsonp: 'jsonp',
         jsonpCallback: "wamResult",
@@ -96,6 +138,27 @@ function wamResult(data) {
             $("#tdBefore").text(String(iBef));
             $("#tdAfter").text(String(iAft));
             $("#divWAM").show();
+            if(document.getElementById("wamMaskFrame") != null)
+            {
+                if(document.getElementById("wamMaskFrame").style.display == "none") {
+                    //스크롤값 계산해서 top left 수정
+                    document.getElementById("wamMaskFrame").style.top = String(window.scrollY || window.pageYOffset) + "px";
+                    document.getElementById("wamMaskFrame").style.left = String(window.scrollX || window.pageXOffset) + "px";
+                    document.getElementById("wamMaskFrame").style.display = "block";
+                }
+            }
+
+            if(document.getElementById("wamMainFrame") != null)
+            {
+                if(document.getElementById("wamMainFrame").style.display == "none") {
+                    //스크롤값 계산해서 top left 수정
+                    var popupY= (window.innerHeight / 2) - (WaitAMinute.MainLayerY / 2);
+                    var popupX = (window.innerWidth / 2) - (WaitAMinute.MainLayerX / 2);
+                    document.getElementById("wamMainFrame").style.top = String((window.scrollY || window.pageYOffset) + popupY) + "px";
+                    document.getElementById("wamMainFrame").style.left = String((window.scrollX || window.pageXOffset) + popupX) + "px";
+                    document.getElementById("wamMainFrame").style.display = "block";
+                }
+            }
             setTimeout(wamWait, 1000);
         }
         else if (data.RtnType == "PASS") {
@@ -103,13 +166,25 @@ function wamResult(data) {
             $("#tdBefore").text(String(iBef));
             $("#tdAfter").text(String(iAft));
             $("#divWAM").hide();
-            WaitAMinute.CallBodyFunction();
-            wamFinish();
+            if(document.getElementById("wamMaskFrame") != null)
+            {
+                document.getElementById("wamMaskFrame").style.display = "none";
+            }
+
+            if(document.getElementById("wamMainFrame") != null)
+            {
+                document.getElementById("wamMainFrame").style.display = "none";
+            }
+
+            setTimeout(function(){
+                WaitAMinute.CallBodyFunction();
+                wamFinish();    
+            }, 500);
+            
         }
         else if (data.RtnType == "DONE") {                
             $("#btnFInish").hide();
             $("#btnStart").show();
-            //alert("끝");
         }
     }
 }
